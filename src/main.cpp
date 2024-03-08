2f92ca8a4e628d08234731dd482b07f49e952b45
Szuszu <nszostakk@gmail.com>
2024-03-08 18:27:43 +0100

    cleaning up main + deleted test file

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,91 @@
 
 #include "GOL.h"
 
-// int gridCountX = 50;
-// int gridCountY = 50;
+class MainComponent{
+    private:
+        SDL_Window *window = nullptr;
+        SDL_Renderer *renderer = nullptr;
 
-// int gridCellSize = 20;
+    public:
+        int windowSizeX;
+        int windowSizeY;
 
-int windowSizeX = (gridCountX * gridCellSize) + 1; 
-int windowSizeY = (gridCountY * gridCellSize) + 1; 
+        void init();
+        void drawGridLines();
+        void drawCells(std::vector<std::vector<int>> arr);
+        void clearRenderer();
+        void updateRenderer();
+        ~MainComponent();
+};
 
-int main(int argc, char *argv[])
-{
-    GameOfLife gol;
-    std::vector<std::vector<int>> out(gridCountX, std::vector<int> (gridCountY, 0));
+MainComponent::~MainComponent(){\
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+}
 
-
-    SDL_Window *window = nullptr;
-    SDL_Renderer *renderer = nullptr;
-
+void MainComponent::init(){
     SDL_Init(SDL_INIT_EVERYTHING);       
+    
+    int windowSizeX = (gridCountX * gridCellSize) + 1; 
+    int windowSizeY = (gridCountY * gridCellSize) + 1; 
+
     SDL_CreateWindowAndRenderer(windowSizeX, windowSizeY, 0, &window, &renderer);
     SDL_RenderSetScale(renderer, 1, 1);
 
-    SDL_SetRenderDrawColor(renderer, 22, 22, 22, 255);      //  Draw background
+    SDL_SetRenderDrawColor(renderer, 22, 22, 22, 255);
     SDL_RenderClear(renderer);
+}
 
+void MainComponent::drawGridLines(){
+    SDL_SetRenderDrawColor(renderer, 44, 44, 44, 255);
+
+    for(int x = 0; x < 1 + gridCountX * gridCellSize; x += gridCellSize){
+        SDL_RenderDrawLine(renderer, x, 0, x, windowSizeY);
+    }
+    for(int y = 0; y < 1 + gridCountY * gridCellSize; y += gridCellSize){
+        SDL_RenderDrawLine(renderer, 0, y, windowSizeX, y);
+    }
+}
+
+void MainComponent::drawCells(std::vector<std::vector<int>> arr){
+
+    for(int x = 0; x < gridCountX; x++){
+        for(int y = 0; y < gridCountY; y++){
+            if(arr[x][y] == 1){
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            }
+            else{
+                SDL_SetRenderDrawColor(renderer, 22, 22, 22, 255);
+            }
+
+            SDL_Rect cell;
+            cell.x = x * gridCellSize;
+            cell.y = y * gridCellSize;
+            cell.w = gridCellSize;
+            cell.h = gridCellSize;
+
+            SDL_RenderFillRect(renderer, &cell);
+        }
+    }
+}
+
+void MainComponent::clearRenderer(){
+    SDL_RenderClear(renderer);
+}
+
+void MainComponent::updateRenderer(){
+    SDL_RenderPresent(renderer);
+}
+
+int main(int argc, char *argv[])
+{
+    MainComponent mainComponent;
+    GameOfLife gol;
+
+    std::vector<std::vector<int>> out(gridCountX, std::vector<int> (gridCountY, 0));
+
+
+    mainComponent.init();
     gol.init();
 
     // Infinite loop for application
@@ -37,66 +98,25 @@ int main(int argc, char *argv[])
     while(gameIsRunning){
 
         SDL_Event event;
-        //  Start event loop
         while(SDL_PollEvent(&event)){
-            // Handle each specific loop
             if(event.type == SDL_QUIT){
                 gameIsRunning = false;
             }
         }
+
         usleep(100000);
-        // system("clear");
 
         out = gol.update();
         
-        SDL_RenderClear(renderer);
-        // Drawing singular cells
-        for(int x = 0; x < gridCountX; x++){
-            for(int y = 0; y < gridCountY; y++){
-                // if(out[x][y] == 1)          std::cout<<'#';
-
-                // else                        std::cout<<' ';
-                
-                if(out[x][y] == 1){
-                    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-                }
-                else{
-                    SDL_SetRenderDrawColor(renderer, 22, 22, 22, 255);
-                }
-                // SDL_SetRenderDrawColor(renderer, 44, 44, 44, 255);
-                SDL_Rect cell;
-                cell.x = x * gridCellSize;
-                cell.y = y * gridCellSize;
-                cell.w = gridCellSize;
-                cell.h = gridCellSize;
-
-                SDL_RenderFillRect(renderer, &cell);
-                
-            }
-            // std::cout<<"\n";
-            
-        }
-        // SDL_RenderPresent(renderer);
-
-        // SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 44, 44, 44, 255);      //  Draw grid lines
-
-        for(int x = 0; x < 1 + gridCountX * gridCellSize; x += gridCellSize){
-            SDL_RenderDrawLine(renderer, x, 0, x, windowSizeY);
-        }
-
-        for(int y = 0; y < 1 + gridCountY * gridCellSize; y += gridCellSize){
-            SDL_RenderDrawLine(renderer, 0, y, windowSizeX, y);
-        }
+        mainComponent.clearRenderer();
+        mainComponent.drawCells(out);
+        mainComponent.drawGridLines();
+        mainComponent.updateRenderer();
 
 
         gol.swapArrays();
-        SDL_RenderPresent(renderer);
     }
 
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
-  return 0;
+    SDL_Quit();
+    return 0;
 }
